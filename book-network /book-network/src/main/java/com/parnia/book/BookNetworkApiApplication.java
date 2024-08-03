f8a3c8813d708f51c3e569712c12f2b904d21a9c
parnia <parnia@parnia>
2024-08-03 23:27:51 +0330

    feat(domain): add User, Token, Role domains

--- book-network /book-network/src/main/java/com/parnia/book/BookNetworkApiApplication.java
+++ book-network /book-network/src/main/java/com/parnia/book/BookNetworkApiApplication.java
@@ -2,12 +2,14 @@ package com.parnia.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BookNetworkApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookNetworkApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookNetworkApiApplication.class, args);
+    }
 
 }
---
+++ book-network /book-network/src/main/java/com/parnia/book/user/Token.java
@@ -0,0 +1,28 @@
+package com.parnia.book.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String token;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    private LocalDateTime expiresAt;
+    private LocalDateTime validateAt;
+    private LocalDateTime createdAt;
+}
