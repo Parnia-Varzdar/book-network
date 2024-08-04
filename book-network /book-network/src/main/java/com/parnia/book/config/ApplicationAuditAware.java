a59b32f544eace7d5c72e4cd8dc71a22a5bb6fe5
parnia <parnia@parnia>
2024-08-04 17:09:50 +0330

    feat(project): implement application audit aware

---
+++ book-network /book-network/src/main/java/com/parnia/book/config/ApplicationAuditAware.java
@@ -0,0 +1,20 @@
+package com.parnia.book.config;
+
+import com.parnia.book.user.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class ApplicationAuditAware implements AuditorAware<Integer> {
+    @Override
+    public Optional<Integer> getCurrentAuditor() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
+            return Optional.empty();
+        }
+        var userPrincipal = (User) authentication.getPrincipal();
+        return Optional.ofNullable(userPrincipal.getId());
+    }
+}
