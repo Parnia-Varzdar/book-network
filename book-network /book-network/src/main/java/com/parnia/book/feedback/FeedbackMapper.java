f5f64563341b2b4b3f5b1797e313fab3c6040ac5
parnia <parnia@parnia>
2024-08-06 22:37:34 +0330

    feat(feedback): Implement business logic of Feedback domain

---
+++ book-network /book-network/src/main/java/com/parnia/book/feedback/FeedbackMapper.java
@@ -0,0 +1,25 @@
+package com.parnia.book.feedback;
+
+import com.parnia.book.book.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class FeedbackMapper {
+    public Feedback toFeedback(FeedbackRequest request) {
+        return Feedback.builder()
+                .note(request.note())
+                .comment(request.comment())
+                .book(Book.builder().id(request.bookId()).build())
+                .build();
+    }
+
+    public FeedbackResponse toFeedbackResponse(Feedback feedback, Integer userId) {
+        return FeedbackResponse.builder()
+                .note(feedback.getNote())
+                .comment(feedback.getComment())
+                .ownFeedback(Objects.equals(feedback.getCreatedBy(), userId))
+                .build();
+    }
+}
